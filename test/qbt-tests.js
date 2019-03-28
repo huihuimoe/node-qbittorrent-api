1d32f1bfef0206a23813baa15954621f847a65c0
huihuimoe <0w0@loli.pet>
2019-03-28 18:24:15 +0800

    fix: lint

--- test/qbt-tests.js
+++ test/qbt-tests.js
@@ -1,29 +1,29 @@
-﻿var assert = require("assert");
-var qBittorrent = require("..");
+﻿const assert = require('assert');
+const qBittorrent = require('..');
 
-var session = qBittorrent.connect("localhost:9091");
-session.version(function (error, data) {
+const session = qBittorrent.connect('localhost:9091');
+session.version(function(error, data) {
   assert.ifError(error);
-  assert.ok(data.startsWith("v"));
-  session.active(function (error, data) {
+  assert.ok(data.startsWith('v'));
+  session.active(function(error, data) {
     assert.ifError(error);
-    var item = data[0];
+    const item = data[0];
     assert.ok(item.hash);
-    session.details(item, function (error, data) {
+    session.details(item, function(error, data) {
       assert.ifError(error);
       assert.equal(item.hash, data[0].hash);
     });
-    var label = item.label;
-    session.setLabel(item.hash, "test", function (error) {
+    const label = item.label;
+    session.setLabel(item.hash, 'test', function(error) {
       assert.ifError(error);
-      session.search(item.name, function (error, data) {
+      session.search(item.name, function(error, data) {
         assert.ifError(error);
-        var flag = false;
-        data.forEach(function (item2) {
+        let flag = false;
+        data.forEach(function(item2) {
           if (item2.name === item.name) {
             flag = true;
             assert.equal(item.hash, item2.hash);
-            assert.equal(item2.label, "test");
+            assert.equal(item2.label, 'test');
             session.setLabel(item2, label);
           }
         });
